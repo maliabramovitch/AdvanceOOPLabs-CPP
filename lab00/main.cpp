0fbc9f4f9f76a23367963ce417b55ec81af32c9a
maliabramovitch <107685274+maliabramovitch@users.noreply.github.com>
2023-03-09 14:48:45 +0200

    3

--- lab00/main.cpp
+++ lab00/main.cpp
@@ -18,11 +18,28 @@ void reversePrint(char *fileName) {
     unique_copy(v.rbegin(), v.rend(), out_it);
 }
 
+template<class InIter, class OutIter, class Comparator>
+void SortCopy(InIter first, InIter last, OutIter result, Comparator cmp) {
+    InIter tFirst = first;
+    OutIter tResult = result;
+    while (tFirst != last) {
+        *tResult++ = *tFirst++;
+    }
+    for (OutIter tmp(result + 1); distance(tmp,tResult) != 0; ++tmp) {
+        OutIter swappi(tmp);
+        while (swappi != result && cmp(*swappi, *(swappi-1))) {
+            swap(*swappi,*(swappi-1));
+            --swappi;
+        }
+    }
+}
+
+
 int main(int argc, char **argv) {
 /**
  * rational testing
  */
-    rational *r1 = new rational(2, 4);
+/*    rational *r1 = new rational(2, 4);
     rational *r2 = new rational(2, 3);
     string str = "1/2";
     rational *r3 = new rational(str);
@@ -46,14 +63,28 @@ int main(int argc, char **argv) {
     }
     catch (...) {
         cout << "DevisionByZero" << endl;
-    }
+    }*/
 /**
  * reversePrint
  */
-    reversePrint(argv[1]);
+    /*reversePrint(argv[1]);*/
 /**
  *
  */
+    const int SIZE = 64;
+    const int UBound = 101;
+    vector<int> V, T(SIZE);
+    srand(time(NULL));
+    for (int i = 0; i < SIZE; i++)
+        V.push_back(rand() % UBound);
+    ostream_iterator<int> out(cout, " ");
+    copy(V.begin(), V.end(), out);
+    cout << "\r\n";
+    int Nelem = distance(V.begin(), V.end());
+    cout << "Sorting 16 first elements out of " << Nelem << "\r\n";
+    SortCopy(V.begin(), V.begin() + 15, T.begin(), less<int>());
+    copy(T.begin(), T.end(), out);
+    cout << "\r\n";
     return 0;
 }
 
