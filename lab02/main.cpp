c6e8f962ab2015b7f7eab8fde42293265982f8c1
maliabramovitch <107685274+maliabramovitch@users.noreply.github.com>
2023-03-27 10:19:58 +0300

    lab02 1.0

--- lab02/main.cpp
+++ lab02/main.cpp
@@ -7,14 +7,40 @@ using namespace std;
 
 
 template<typename Iter>
-void advaneceHelper(Iter &it, int steps, typename std::iterator_traits<Iter>::ierator_category) {
-    for (int i = steps; i < steps; ++i) {
-        ++it;
+void advaneceHelper(Iter &it, int steps, typename std::input_iterator_tag) {
+    if (steps > 0) {
+        for (int i = steps; i < steps; ++i) {
+            ++it;
+        }
+    } else{
+        cout << "illegible operation" << endl;
     }
 }
 
 template<typename Iter>
-void advaneceHelper(Iter &it, int steps, typename std::bidirectional_iterator_tag) {
+void advaneceHelper(Iter &it, int steps, output_iterator_tag) {
+    if (steps > 0) {
+        for (int i = steps; i < steps; ++i) {
+            ++it;
+        }
+    } else{
+        cout << "illegible operation" << endl;
+    }
+}
+
+template<typename Iter>
+void advaneceHelper(Iter &it, int steps, typename std::forward_iterator_tag) {
+    if (steps > 0) {
+        for (int i = steps; i < steps; ++i) {
+            ++it;
+        }
+    } else{
+        cout << "illegible operation" << endl;
+    }
+}
+
+template<typename Iter>
+void advaneceHelper(Iter &it, int steps, bidirectional_iterator_tag) {
     if (steps < 0) {
         while (steps++) {
             --it;
@@ -28,22 +54,22 @@ void advaneceHelper(Iter &it, int steps, typename std::bidirectional_iterator_ta
 
 
 template<typename Iter>
-void advaneceHelper(Iter &it, int steps, typename std::random_access_iterator_tag) {
+void advaneceHelper(Iter &it, int steps, random_access_iterator_tag) {
     it += steps;
 }
 
 template<typename Iter>
 void advanced(Iter &it, int steps) {
-    advaneceHelper(it, steps, typename std::iterator_traits<Iter>::iterator_category{});
+    advaneceHelper(it, steps, typename iterator_traits<Iter>::iterator_category{});
 }
 
 
 int main() {
-    deque<int> d = {1,2,3,4,5,6,7,8,9,10};
+    deque<int> d = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     deque<int>::iterator it = d.begin();
     advanced(it, 2);
-    cout << *it<<endl;
-    list<int> l = {1,2,3,4,5,6,7,8,9,10};
+    cout << *it << endl;
+    list<int> l = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     list<int>::iterator it2 = l.begin();
     advanced(it2, 4);
     cout << *it2;
